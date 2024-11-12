fc91de57ed7845d3b21c5916fb995da98202a2a9
JcdbTPU40 <g2427040@st.t-kougei.ac.jp>
2024-11-12 11:42:50 +0900

    Created GuessTheNumberGame

---
+++ GuessTheNumberGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GuessTheNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int targetNumber = random.Next(1, 101); // Generate a number between 1 and 100
+
+            bool guessedCorrectly = false;
+            while (!guessedCorrectly)
+            {
+                Console.WriteLine("Guess the number between 1 and 100: ");
+                string userInput = Console.ReadLine();
+                int userGuess;
+
+                if(int.TryParse(userInput, out userGuess))
+                {
+
+                if (userGuess > targetNumber)
+                {
+                    Console.Write("Too High!");
+                }
+                else if (userGuess < targetNumber)
+                {
+                    Console.WriteLine("Too Low!");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the number.");
+                    guessedCorrectly = true;
+                }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                }
+            }
+        }
+    }
+}
--- GuessTheNumberGame/obj/Debug/net8.0/GuessTheNumberGame.AssemblyInfo.cs
+++ GuessTheNumberGame/obj/Debug/net8.0/GuessTheNumberGame.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("GuessTheNumberGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d27f97a7af23c4ccb1f11c5956130084632a50e")]
-[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyProductAttribute("GuessTheNumberGame")]
+[assembly: System.Reflection.AssemblyTitleAttribute("GuessTheNumberGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment クラスによって生成されました。
